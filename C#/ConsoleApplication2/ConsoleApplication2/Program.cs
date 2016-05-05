dd14f23e98a0043e07777f171c9b0c35b4bae225
Lalit Singh <lalitsinghb1@gmail.com>
2016-05-05 20:21:35 +0530

    cs

---
+++ C#/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "f";
+            string k;
+            
+            for(;;){
+                k = Console.ReadKey().Key.ToString();
+                Console.Write("\n" + k +"\n");
+                if (string.ReferenceEquals(k,a)) { Console.WriteLine("break");}
+            }
+            Console.ReadKey();
+        }
+    }
+}
